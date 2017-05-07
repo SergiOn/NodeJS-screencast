b53d9bfccf8a4d7cd89029cdfed7d64abe309805
SergiOn <Onishhenko@gmail.com>
2017-05-07 22:27:34 +0300

    #12-top-3-inherit-error # my Error class

--- top/error.js
+++ top/error.js
@@ -5,19 +5,53 @@ const phrases = {
     'world': 'мир'
 };
 
+
+function PhraseError(message) {
+    this.message = message;
+}
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = 'PhraseError';
+
+function HttpError(status, message) {
+    this.status = status;
+    this.message = message;
+}
+util.inherits(HttpError, Error);
+HttpError.prototype.name = 'HttpError';
+
+
 function getPhrase(name) {
     if (!phrases[name]) {
-        throw new Error('We haven`t phrase:', name);
+        throw new PhraseError(`We haven\`t phrase: ${name}`); // HTTP 500
     }
     return phrases[name];
 }
 
 function makePage(url) {
     if (url !== 'index.html') {
-        throw new Error('We haven`t that page');
+        throw new HttpError(404, 'We haven`t that page'); // HTTP 404
     }
-    return util.format('%s, %s!', getPhrase('Hello'), getPhrase('world'))
+    return util.format('%s, %s!', getPhrase('Hell'), getPhrase('world'))
 }
 
-let page = makePage('index.html');
-console.log(page);
+try {
+    let page = makePage('index');
+    console.log(page);
+} catch (e) {
+    if (e instanceof HttpError) {
+        console.log(e.status, e.message);
+    } else {
+        console.error(e);
+    }
+}
+
+try {
+    let page = makePage('index.html');
+    console.log(page);
+} catch (e) {
+    if (e instanceof HttpError) {
+        console.log(e.status, e.message);
+    } else {
+        console.error('Error %s\n message: %s\n stack: %s', e.name, e.message, e.stack);
+    }
+}
