f92e4df5105bc90011c8a21916b4d2a837b8f867
SergiOn <Onishhenko@gmail.com>
2017-05-10 01:17:01 +0300

    #13-top-4-eventemitter # memory with on

---
+++ top/leak2.js
@@ -0,0 +1,20 @@
+const EventEmitter = require('events').EventEmitter;
+
+const db = new EventEmitter;
+
+function Request() {
+    this.bigData = new Array(1e6).join('*');
+
+    this.send = (data) => {
+        console.log(data);
+    };
+
+    db.on('data', (info) => {
+        this.send(info);
+    }); // db.emit
+}
+
+setInterval(() => {
+    const request = new Request();
+    console.log(process.memoryUsage().heapUsed);
+}, 200);
