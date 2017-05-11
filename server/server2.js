7876bbc2bb434d6e2212fd6e7a463d90f0bd4387
SergiOn <Onishhenko@gmail.com>
2017-05-12 00:12:30 +0300

    #15-server-2-echo # res.end

--- server/server2.js
+++ server/server2.js
@@ -6,10 +6,15 @@ const url = require('url');
 const server = new http.Server((req, res) => {
     console.log(req.method, req.url);
 
-    const urlParse = url.parse(req.url);
+    const urlParse = url.parse(req.url, true);
     console.log(urlParse);
 
-    res.end();
+    if (urlParse.pathname === '/echo' && urlParse.query.message === 'hello') {
+        res.end( urlParse.query.message );
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
 });
 
 server.listen(1337, '127.0.0.1');
