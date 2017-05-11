510b661aec5f5c2f6760028785e6755e2d5da6ae
SergiOn <Onishhenko@gmail.com>
2017-05-12 00:06:23 +0300

    #15-server-2-echo # url.parse

--- server/server2.js
+++ server/server2.js
@@ -1,7 +1,15 @@
+//  http://localhost:1337/echo?message=hello
+
 const http = require('http');
+const url = require('url');
 
 const server = new http.Server((req, res) => {
     console.log(req.method, req.url);
+
+    const urlParse = url.parse(req.url);
+    console.log(urlParse);
+
+    res.end();
 });
 
 server.listen(1337, '127.0.0.1');
