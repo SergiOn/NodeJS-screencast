cab8b0d07d23e4617c75491bab2f115c05b0ae16
SergiOn <Onishhenko@gmail.com>
2017-05-12 00:18:54 +0300

    #15-server-2-echo # res.statusCode

--- server/server2.js
+++ server/server2.js
@@ -7,19 +7,18 @@ const server = new http.Server((req, res) => {
 
     console.log( req.headers );
 
-    /*
     console.log(req.method, req.url);
 
     const urlParse = url.parse(req.url, true);
     console.log(urlParse);
 
     if (urlParse.pathname === '/echo' && urlParse.query.message === 'hello') {
+        res.statusCode = 200; // OK
         res.end( urlParse.query.message );
     } else {
-        res.statusCode = 404;
+        res.statusCode = 404; // Not Found
         res.end('Page not found');
     }
-    */
 });
 
 server.listen(1337, '127.0.0.1');
