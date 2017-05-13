07d8fb6be6be23c51829362f7c01bf0d8a2fb457
SergiOn <Onishhenko@gmail.com>
2017-05-13 23:36:48 +0300

    #23-fs-1-fs # with error

--- fs/read.js
+++ fs/read.js
@@ -5,9 +5,21 @@ fs.readFile(__filename, (err, data) => {
         console.log(err);
     } else {
         console.log(data);
-        console.log(data.toString());
+        // console.log(data.toString());
         console.log(data[0]);
         console.log(data[1]);
         console.log(data.length);
+    }
+});
+
+fs.readFile('dsds', {encoding: 'utf-8'}, (err, data) => {
+    if (err) {
+        if (err.code === 'ENOENT') {
+            console.log(err.message);
+        } else {
+            console.log(err);
+        }
+    } else {
+        console.log(data);
     }
 });
