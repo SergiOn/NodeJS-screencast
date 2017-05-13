ed372e3503645b82c0b53b531fd33050fe3eba31
SergiOn <Onishhenko@gmail.com>
2017-05-14 00:04:03 +0300

    #23-fs-1-fs # fs.writeFile

--- fs/read2.js
+++ fs/read2.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 
+/*
 fs.stat(__filename, (err, stats) => {
     console.log(stats.isFile());
     console.log(stats);
+});
+*/
+
+
+fs.writeFile('file.tmp', 'data', (err) => {
+    if (err) throw err;
+
+    fs.rename('file.tmp', 'new.tmp', (err) => {
+        if (err) throw err;
+
+        fs.unlink('new.tmp', (err) => {
+            if (err) throw err;
+        });
+    });
 });
