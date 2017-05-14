7b0ccc4214e521890cec908c80e7319a78d285ff
SergiOn <Onishhenko@gmail.com>
2017-05-14 16:10:57 +0300

    #26-streams-2-net # file.pipe

---
+++ streams/pipe2.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+new http.Server((req, res) => {
+
+    if (req.url === '/big.html') {
+        let file = new fs.ReadStream('streams/big.html');
+        sendFile(file, res);
+    }
+
+}).listen(3000);
+
+function sendFile(file, res) {
+    file.pipe(res);
+}
+
