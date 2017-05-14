a16484fcb3985ca5aeef1550e856e3b48c6ad98e
SergiOn <Onishhenko@gmail.com>
2017-05-14 17:42:30 +0300

    #27-long-poll-chat # update chat

--- long-poll-chat/chat.js
+++ long-poll-chat/chat.js
@@ -4,6 +4,10 @@ exports.subscribe = function(req, res) {
     console.log("subscribe");
 
     clients.push(res);
+
+    res.on('close', function() {
+        clients.splice(clients.indexOf(res), 1);
+    });
 };
 
 exports.publish = function(message) {
@@ -16,4 +20,8 @@ exports.publish = function(message) {
 
     clients = [];
 };
+
+setInterval(function() {
+    console.log(clients.length);
+}, 2000);
 
