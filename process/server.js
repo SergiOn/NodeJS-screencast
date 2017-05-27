10f940180ada4212fa90eb14e8ec515e724cba30
SergiOn <Onishhenko@gmail.com>
2017-05-27 20:28:42 +0300

    #29-process-params # several process params

--- process/server.js
+++ process/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
+const opts = require('optimist').argv;
 
 http.createServer((req, res) => {
 
+    // console.log(process.argv);
+    // console.log(opts);
+    // console.log(process.env);  // process.env.NODE_ENV
+
     res.end('The server is running!');
 
-}).listen(3000);
+}).listen(opts.port);
 
