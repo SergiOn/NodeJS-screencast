9dd81786735720394ad07fa511baf60af84db176
SergiOn <Onishhenko@gmail.com>
2017-05-28 16:25:23 +0300

    #31-chat-2 # packeges

--- chat/app.js
+++ chat/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const config = require('./config');
 const port = config.get('port');
 const log = require('./libs/log')(module);
@@ -10,36 +15,24 @@ const app = express();
 app.set('port', port);
 
 http.createServer(app).listen(port, () => {
-    log.info('Express server listening on port ' + port);
+    log.info('Express server listening on port', port);
 });
 
-app.use((req, res, next) => {
-    if (req.url === '/') {
-        res.end('hello');
-    } else {
-        next();
-    }
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use((req, res, next) => {
-    if (req.url === '/forbidden') {
-        next(new Error("wops, denied"));
-    } else {
-        next();
-    }
-});
-
-app.use((req, res, next) => {
-    if (req.url === '/test') {
-        res.end('test');
-    } else {
-        next();
-    }
-});
-
-app.use((req, res) => {
-    res.status(404).send('Page Not Found Sorry');
-});
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+} else {
+    app.use(logger('default'));
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(session());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((err, req, res, next) => {
     // NODE_ENV = 'production'
@@ -52,30 +45,9 @@ app.use((err, req, res, next) => {
 });
 
 /*
-var express = require('express');
-
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
@@ -97,7 +69,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
-
 */
