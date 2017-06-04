57a5a2a35275c62dd4f183a84627ba631ee19688
SergiOn <Onishhenko@gmail.com>
2017-06-04 23:22:50 +0300

    #37-chat-7 # users

--- chat/app.js
+++ chat/app.js
@@ -37,8 +37,14 @@ app.use(cookieParser());
 // app.use(session());
 
 app.get('/', (req, res, next) => {
-    res.render('index', {
+    res.render('index');
+});
 
+const User = require('./models/user').User;
+app.get('/users', function (req, res, next) {
+    User.find({}, function (err, users) {
+        if (err) throw err;
+        res.json(users);
     });
 });
 
@@ -52,30 +58,3 @@ app.use((err, req, res, next) => {
         res.send(500);
     }
 });
-
-/*
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
